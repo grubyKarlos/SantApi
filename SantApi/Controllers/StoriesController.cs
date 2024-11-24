d3b33ed5b5be0fd1259dce40608f79b10955f994
jons <jons@PJ-LEGION>
2024-11-24 15:36:28 +0100

    Dodaj pliki projektów.

---
+++ SantApi/Controllers/StoriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StoriesController : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok();
+    }
+}
